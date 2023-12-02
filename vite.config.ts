980762780f52fc458496683ca40612acdf8e7360
Galos <gal135710@gmail.com>
2023-12-03 01:45:57 +0200

    change to start from right path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	base: "/CatanTrackApp/", // Replace 'my-vite-project' with your repo name
+	plugins: [react()],
+});
