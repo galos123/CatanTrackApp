f721f16cfbedbef6aaa6ebfd563ae6852c76a290
Galos <gal135710@gmail.com>
2023-12-03 02:13:05 +0200

    changed build to be in docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	base: "/CatanTrackApp/", // Replace 'my-vite-project' with your repo name
+	base: "/CatanTrackApp/", // Replace with your repository name
 	plugins: [react()],
+	build: {
+		outDir: "docs",
+	},
 });
